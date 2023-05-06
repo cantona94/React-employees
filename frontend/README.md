8b4383a3a75ab842f9b424c977eb44add744ab8d
cantona94 <vlstan@mail.ru>
2023-05-06 22:27:12 +0500

    add React, Redux Toolkit

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# React, TypeScript, Redux Toolkit
---
+++ frontend/src/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { store } from './app/store';
+import reportWebVitals from './reportWebVitals';
+import { Paths } from './paths'
+import './index.css';
+import { Register, Login } from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <Login />,
+  },
+  {
+    path: Paths.register,
+    element: <Register />,
+  },
+])
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
