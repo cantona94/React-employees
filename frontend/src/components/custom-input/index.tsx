6bb66aab5eb29e5387e1835e1ad9e9b7662f9984
cantona94 <vlstan@mail.ru>
2023-05-14 20:54:01 +0500

    login page

---
+++ frontend/src/components/custom-input/index.tsx
@@ -0,0 +1,27 @@
+import { Form, Input } from "antd";
+
+type Props = {
+    name: string;
+    placeholder: string;
+    type?: string;
+};
+
+export const CustomInput = ({
+    type = 'text',
+    name,
+    placeholder,
+}: Props) => {
+    return (
+        <Form.Item
+            name={name}
+            rules={[{ required: true, message: "Обязательное поле" }]}
+            shouldUpdate={true}
+        >
+            <Input
+                placeholder={placeholder}
+                type={type}
+                size="large"
+            />
+        </Form.Item>
+    )
+}
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import { Layout } from '../components/layout'
+import { Row, Card, Form, Space, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { Layout } from '../components/layout';
+import { CustomInput } from '../components/custom-input';
+import { PasswordInput } from '../components/password-input';
+import { CustomButton } from "../components/custom-button";
+import { Paths } from "../paths";
 
 export const Login = () => {
   return (
     <Layout>
-      <div>Login</div>
+      <Row align="middle" justify="center">
+        <Card title="Войдите" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <CustomButton type="primary" htmlType="submit">
+              Войти
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+          <Typography.Text>
+              Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   )
 }
