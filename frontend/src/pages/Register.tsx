4d516dc43067060fd1360c1ec9aeac1ed5dd77a9
cantona94 <vlstan@mail.ru>
2023-05-15 21:19:39 +0500

    register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Row, Card, Form, Space, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { Layout } from '../components/layout';
+import { CustomInput } from '../components/custom-input';
+import { PasswordInput } from '../components/password-input';
+import { CustomButton } from "../components/custom-button";
+import { Paths } from "../paths";
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Зарегистрируйтесь" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput name="name" placeholder="Имя" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <PasswordInput name="confirmPassword" placeholder="Повторите пароль" />
+            <CustomButton type="primary" htmlType="submit">
+              Зарегистрироваться
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Уже зарегистрированы? <Link to={Paths.login}>Войдите</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
   )
 }
